62914b3be000ba4bd11db9b1ae4cf130f5c17ed7
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-03 15:27:02 +0530

    Refactor FunctionalInterfaceDemo to clean up commented code and streamline lambda expression usage

--- FunctionalInterfaceDemo.java
+++ FunctionalInterfaceDemo.java
@@ -19,31 +19,39 @@ class B{
 
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
+        /*
+        
+
         I i1 = new A();
-
+        
         i1.abc();   
-
+        
         I i2 = new I(){
             public void abc(){
                 System.out.println("From Anonymous class abc() method");
             }
         };
-
+        
         i2.abc();
-
+        
         B b1 = new B(){
             public void demo(){
                 System.out.println("From Anonymous class abc() method");
             }
         };
-
+        
         b1.demo();
-
+        
         I i3 = ()->{
             System.out.println("From Lamba expression");
         };
         
         i3.abc();
+        
+        */
+
+
+        I i3 = ()->System.out.println("From Lambda Function");
+        i3.abc();
     }   
 }
- 
